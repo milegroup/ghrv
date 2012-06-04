6cca9d20fcd2b9f2ea2ca516c19a4e4494cd5009
Leandro Rodríguez-Liñares <leandroide@gmail.com>
2012-06-04 09:40:38 +0200

    About dialog in separated file

---
+++ AboutDlg.py
@@ -0,0 +1,49 @@
+import wx
+from sys import platform
+from configvalues import *
+
+class AboutDlg(wx.Frame):
+    """About box"""
+    
+    import wx.html
+ 
+    def __init__(self, parent, id):
+        if platform != 'darwin':
+            wx.Frame.__init__(self, parent, wx.ID_ANY, title="About", size=aboutWindowSize)
+            self.SetMinSize(aboutWindowMinSize)
+        else:
+            wx.Frame.__init__(self, parent, wx.ID_ANY, title="About", size=aboutWindowSizeMac)
+            self.SetMinSize(aboutWindowSizeMac)
+        
+        self.WindowParent=parent
+        self.Bind(wx.EVT_CLOSE,self.OnEnd)
+        
+        html=wx.html.HtmlWindow(self, id)
+        html.SetPage(
+            '<p align="center"><img src="LogoSmall.png"/></p>'
+            '<h3 align="center">gHRV 0.20</h3>'
+            '<p align="center"><b>gHRV: a graphical application for Heart Rate Variability analysis</b></p>'
+            '<p align="center">Copyright (C) 2012  Milegroup - Dpt. Informatics - University of Vigo - Spain</p>'
+            '<p align="center"><i>http://www.milegroup.net</i></p>'
+            "<hr/>"
+            '<p align="left"><b>Authors:</b></p>'
+            '<ul>'
+            '<li>Leandro Rodr&iacute;guez-Li&ntilde;ares</li>'
+            '<li>Arturo M&eacute;ndez</li>'
+            '<li>Mar&iacute;a Jos&eacute; Lado</li>'
+            '<li>Xos&eacute; Ant&oacute;n Vila</li>'
+            '</ul>'
+            "<hr/>"
+            '<p align="justify"> This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p>'
+            '<p align="justify">This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details (http://www.gnu.org/licenses/).</p>'
+            )
+        
+        self.Show()
+        self.Layout()
+        #self.Centre()
+        
+        
+    def OnEnd(self,event):
+        self.WindowParent.OnAboutEnded()
+        self.Destroy()
+        
