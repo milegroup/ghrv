6edf44e2614d5d9db72834bca397448e4a7812b4
Leandro Rodríguez-Liñares <leandroide@gmail.com>
2013-01-24 16:37:47 +0100

    Exception is raised when there are not enough frames

---
+++ Exceptions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+#   ----------------------------------------------------------------------
+#   gHRV: a graphical application for Heart Rate Variability analysis
+#   Copyright (C) 2012  Milegroup - Dpt. Informatics
+#      University of Vigo - Spain
+#      www.milegroup.net
+#
+#   Authors:
+#     - Leandro Rodríguez-Liñares
+#     - Arturo Méndez
+#     - María José Lado
+#     - Xosé Antón Vila
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   ----------------------------------------------------------------------
+
+class Error(Exception):
+    pass
+
+class FewFramesError(Error):
+    def __init__(self,NumOfFrames):
+        self.NumOfFrames = NumOfFrames
+  
