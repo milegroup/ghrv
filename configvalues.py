5482552e7e0b24341b7a851501bcc4a8f171408d
Leandro Rodríguez <leandroide@gmail.com>
2013-11-27 18:24:28 +0000

    Version 1.5

--- configvalues.py
+++ configvalues.py
@@ -31,7 +31,7 @@ DebugMode = False
 ReportVersion = False
 ColoredButtons = True
 ColoredBGPlots = True
-Version="1.4" # Things like 1.0.5 are not valid
+Version="1.5" # Things like 1.0.5 are not valid
 
 borderBig=10
 borderSmall=5
